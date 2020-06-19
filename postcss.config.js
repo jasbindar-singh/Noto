b5db55a6d279f14b5c0487447c5eef8a21676dcf
Jasbindar Singh <Jasbindar Singh>
2020-06-19 16:47:52 +0530

    removed unused css purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,20 @@
 const tailwindcss = require('tailwindcss');
 
-module.exports = {
-    plugins: [
-      tailwindcss('./tailwind.config.js'),
-      require('autoprefixer'),
-    ]
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.jsx',
+  ],
+  defaultExtractor: content => {
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+    return broadMatches.concat(innerMatches)
   }
+})
+  
+module.exports = {
+  plugins: [
+    tailwindcss('./tailwind.config.js'),
+    require('autoprefixer'),
+    purgecss
+  ]
+}
